14132e8633a30a3d54db2dc33426446feec0a24e
Dimitrov Bernát <hrutkamargit@gmail.com>
2024-03-23 17:44:22 +0100

    +1 h1 sor
    sd

--- main_core_test/obj/Debug/net8.0/main_core_test.AssemblyInfo.cs
+++ main_core_test/obj/Debug/net8.0/main_core_test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main_core_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6229653f800b1a7faa3f4e159b24f9a18bb5fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6386cd86799465e03deffc6da963a8166e94896b")]
 [assembly: System.Reflection.AssemblyProductAttribute("main_core_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main_core_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
