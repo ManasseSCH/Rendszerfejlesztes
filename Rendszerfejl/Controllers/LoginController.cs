73fa6bf122706bdc92e3c7ec8da92b51b576ddd2
GreedyRegex <stefanbalazs42@gmail.com>
2024-04-01 19:43:05 +0200

    bejelentkezes
    bejelentkezes

---
+++ Rendszerfejl/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rendszerfejl.Models;
+
+namespace Rendszerfejl.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.userName == "asd" && userModel.password == "asd")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Rendszerfejl/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Rendszerfejl.Models
+{
+    public class UserModel
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public string name { get; set; }    
+        public string password { get; set; }        
+    }
+}
