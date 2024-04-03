6ab34e9ddc552e0dac01e7f5d566e84a5e2423a0
Dimitrov Bernát <hrutkamargit@gmail.com>
2024-04-03 11:34:32 +0200

    Topic API megvalositasa
    Topic controller teljesen kesz
    TODO: kommentekhez is API

---
+++ Rendszerfejl/Controllers/TopicControllerAPI.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rendszerfejl.Models;
+using Rendszerfejl.Services;
+using System.Net.Http.Headers;
+
+namespace Rendszerfejl.Controllers
+{
+    [ApiController]
+    [Route("api/TopicControllerAPI")]
+    public class TopicControllerAPI : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult <IEnumerable<TopicModel>> Index()
+        {
+            TopicsDAO topicsDAO = new TopicsDAO();
+            return topicsDAO.GetAllTopics();
+        }
+
+        [HttpGet("searchfor/{searchTerm}")]
+        public ActionResult <IEnumerable<TopicModel>> SearchResults(string searchTerm) 
+        {
+            TopicsDAO topics = new TopicsDAO();
+            List<TopicModel> topicList = topics.SearchTopics(searchTerm);  
+            
+            return topicList;
+        }
+        /*public IActionResult SearchForm(int id) // Nem lehet megvalósítani (nem is kell, ki is törölhetném, példa kedvéért hagytam itt)
+        {
+            return View();
+        } */
+    }
+}
