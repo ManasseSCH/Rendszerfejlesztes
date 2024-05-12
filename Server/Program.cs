235b462f72bf9d9ff14cca493891d8e6a901e1cf
GreedyRegex <stefanbalazs42@gmail.com>
2024-05-12 15:20:02 +0200

    websocket

--- Server/Program.cs
+++ Server/Program.cs
@@ -9,64 +9,76 @@ using Swashbuckle.AspNetCore.Filters;
 using System.Text;
 
 
-var builder = WebApplication.CreateBuilder(args);
-
-    // Your database operations here
-
-// Add services to the container.
-//BloggingContext bloggingContext = new BloggingContext();
-//Microsoft.EntityFrameworkCore.DbContextOptionsBuilder DbCOB= new Microsoft.EntityFrameworkCore.DbContextOptionsBuilder();
-//bloggingContext.configure_db_manually(DbCOB);
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+namespace Server
 {
-    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    public class Program
     {
-        Description = "Standard Authorization Header using the Bearer scheme(\"bearer{token}\")",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-    options.OperationFilter<SecurityRequirementsOperationFilter>();
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-});
+            // Your database operations here
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(options => {
-            options.TokenValidationParameters = new TokenValidationParameters
+            // Add services to the container.
+            //BloggingContext bloggingContext = new BloggingContext();
+            //Microsoft.EntityFrameworkCore.DbContextOptionsBuilder DbCOB= new Microsoft.EntityFrameworkCore.DbContextOptionsBuilder();
+            //bloggingContext.configure_db_manually(DbCOB);
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddWebSocketManager();// hozzáadja a WebSocketManager-t
+            builder.Services.AddSwaggerGen(options =>
             {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
-                ValidateIssuer = false,
-                ValidateAudience = false
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization Header using the Bearer scheme(\"bearer{token}\")",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
 
-            };
-        } );
+            });
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                            .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+                            ValidateIssuer = false,
+                            ValidateAudience = false
+
+                        };
+                    });
 
 
-var app = builder.Build();
+            var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+            var serviceScopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();//A WebSocketManager használatához szükséges
+            var serviceProvider = serviceScopeFactory.CreateScope().ServiceProvider;//A WebSocketManager használatához szükséges
+
+            app.UseWebSockets(); // Enable WebSocket support
+
+            app.MapWebSocketManager("/ws", serviceProvider.GetService<HelloWorldHandler>());//A WebSocketManager-t használjuk a HelloWorldHandlerrel
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseAuthentication();
-
-app.UseAuthorization();
-var serviceScopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();//A WebSocketManager használatához szükséges
-var serviceProvider = serviceScopeFactory.CreateScope().ServiceProvider;//A WebSocketManager használatához szükséges
-
-app.UseWebSockets(); // Enable WebSocket support
-
-app.MapWebSocketManager("/ws", serviceProvider.GetService<HelloWorldHandler>());//A WebSocketManager-t használjuk a HelloWorldHandlerrel
-
-app.MapControllers();
-
-app.Run();
